41678ab59394e1839bbc366836124b5327adfe62
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2021-03-16 08:32:45 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navigator from "./components/Navigator";
+import {
+  createStyles,
+  ThemeProvider,
+  withStyles,
+  WithStyles,
+} from "@material-ui/core/styles";
+import { Hidden, CssBaseline } from "@material-ui/core";
+import theme from "./theme";
+import { Copyright } from "@material-ui/icons";
+import Header from "./components/Header";
 
-function App() {
+const drawerWidth = 256;
+
+const styles = createStyles({
+  root: {
+    display: "flex",
+    minHeight: "100vh",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  app: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+  },
+  main: {
+    flex: 1,
+    padding: theme.spacing(6, 4),
+    background: "#eaeff1",
+  },
+  footer: {
+    padding: theme.spacing(2),
+    background: "#eaeff1",
+  },
+});
+
+export interface AppProps extends WithStyles<typeof styles> {}
+
+function App(props: AppProps) {
+  const { classes } = props;
+
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+
+        <nav className={classes.drawer}>
+          <Hidden smUp implementation="js">
+            <Navigator
+              PaperProps={{ style: { width: drawerWidth } }}
+              variant="temporary"
+              open={mobileOpen}
+              onClose={handleDrawerToggle}
+            />
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Navigator PaperProps={{ style: { width: drawerWidth } }} />
+          </Hidden>
+        </nav>
+        <div className={classes.app}>
+          <Header onDrawerToggle={handleDrawerToggle} />
+          <main className={classes.main}>content</main>
+          <footer className={classes.footer}>
+            <Copyright />
+          </footer>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default withStyles(styles)(App);
